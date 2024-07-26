2996d0105e7a5963319831be39a448af28a1be37
Abhishek Mishra <abhishek.mishra@zopsmart.com>
2024-07-26 12:11:15 +0530

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Abhishek on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
